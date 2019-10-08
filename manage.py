1e6e5778ecdbf26a0df11f2c3ee6e6ee89ea7ed3
50547870+RamonvdW@users.noreply.github.com <ramon@forcie.localdomain>
2019-10-08 18:32:35 +0200

    New: top-level files

---
+++ manage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2019 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+"""
+    Django's command-line utility for administrative tasks.
+"""
+
+import os
+import sys
+from django.core.management import execute_from_command_line
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data1.settings')
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+# end of file
+
---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#  Copyright (c) 2019 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+RED="\e[31m"
+RESET="\e[0m"
+
+OMIT="--omit=data1/wsgi.py"
+ARGS="$*"
+
+coverage erase
+coverage run ./manage.py test --noinput $*  # note: double quotes not supported around $*
+coverage report --skip-covered --fail-under=80 $OMIT
+res=$?
+coverage html -d covhtml --skip-covered $OMIT
+
+#echo "res=$res"
+if [ "$res" -gt 0 ] && [ -z "$ARGS" ]
+then
+    echo -e "$RED"
+    echo "      ==========================="
+    echo "      FAILED: NOT ENOUGH COVERAGE"
+    echo "      ==========================="
+    echo -e "$RESET"
+    firefox covhtml/index.html&
+fi
+
+# end of file
+
