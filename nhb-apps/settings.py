d9420fcf7729b1035924316087d935a9e0029730
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2019-10-21 20:53:07 +0200

    Moved deploy-specific settings to settings_local.py

--- nhb-apps/settings.py
+++ nhb-apps/settings.py
@@ -32,14 +32,14 @@ SITE_VERSIE = 'test 2019-10-19'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # TODO: uitzoeken waarom en hoe dit in elkaar steekt
-SECRET_KEY = '' #PRIVATE
+#-SECRET_KEY = 'a=^8025bv$o+%$qve3&v5d$gxnx-o640#5!$rxk4g*!73wofs7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#- DEBUG = True
 
-ALLOWED_HOSTS = [
-#PRIVATE
-]
+#- ALLOWED_HOSTS = [
+#-    '127.0.0.1'
+#-]
 
 # for debug_toolbar
 INTERNAL_IPS = [
@@ -121,16 +121,16 @@ WSGI_APPLICATION = 'nhb-apps.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'data3',
-        'USER': '',     # PRIVATE
-        'PASSWORD': '', # PRIVATE
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+#- DATABASES = {
+#-     'default': {
+#-         'ENGINE': 'django.db.backends.postgresql',
+#-         'NAME': 'some-database-name',
+#-         'USER': 'some-username',
+#-         'PASSWORD': 'some-password',
+#-         'HOST': 'localhost',
+#-         'PORT': '5432'
+#-     }
+#- }
 
 
 # Password validation
@@ -187,6 +187,11 @@ STATICFILES_FINDER = [
 
 # applicatie specifieke settings
 MINIMUM_LEEFTIJD_LID = 5
+
+# import install-specific settings from a separate file
+# that is easy to replace as part of the deployment process
+# provides parametes commented out above with #-
+from .settings_local import *
 
 # end of file
 
