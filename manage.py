4b807983b37736f4e578722fa4dd5c51a173e006
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2019-11-12 12:24:40 +0100

    Editorial

--- manage.py
+++ manage.py
@@ -13,10 +13,10 @@ import os
 import sys
 from django.core.management import execute_from_command_line
 
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nhb-apps.settings')
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
