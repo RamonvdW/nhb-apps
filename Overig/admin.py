7a5a3f085a0d2b69b3a0ea1caa3f1e000f4d706e
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2019-11-16 08:03:37 +0100

    Added filter capability

--- Overig/admin.py
+++ Overig/admin.py
@@ -7,7 +7,13 @@
 from django.contrib import admin
 from .models import SiteFeedback, SiteTijdelijkeUrl
 
-admin.site.register(SiteFeedback)
+
+class SiteFeedbackAdmin(admin.ModelAdmin):
+    # filter mogelijkheid
+    list_filter = ('is_afgehandeld',)
+
+
+admin.site.register(SiteFeedback, SiteFeedbackAdmin)
 admin.site.register(SiteTijdelijkeUrl)
 
 # end of file
