5d677fa2a5257c894f8665bdb3aed228e935d6f5
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2019-12-31 17:31:49 +0100

    Added download memory + avoid import on no change

--- Records/cron/download_and_import_records.sh
+++ Records/cron/download_and_import_records.sh
@@ -8,7 +8,9 @@
 
 NHBAPPS="/var/www/nhb-apps"
 LOGDIR="/var/log/www"
+SPOOLDIR="/var/spool/records"
 TMPDIR="/tmp/downloader"
+RECORDS="/tmp/downloader/records.json"      # note: download_gsheet.py contains the same path
 
 ID=$(id -u)
 if [ $ID -ne 0 ]
@@ -22,15 +24,22 @@ LOG="$LOGDIR/${STAMP}_download_and_import_records.log"
 echo "Logging to: $LOG"
 echo "[INFO] Started" > "$LOG"
 
-RECORDS="$TMPDIR/records.json"      # note: download_gsheet.py contains the same path
-
 # prepare to download
 rm -rf "$TMPDIR"
 mkdir -p "$TMPDIR"
 cp "./downloader_service-account.json" "$TMPDIR/service-account.json"
 
+# calculate the checksum for the latest downloaded file
+LATESTFILE=$(ls -1t "$SPOOLDIR"/records_*.json | head -1)
+LATESTHASH="none"
+if [ -e "$LATESTFILE" ]
+then
+    LATESTHASH=$(sha1sum < "$LATESTFILE")
+    echo "[INFO] Hash of previously downloaded records: $LATESTHASH" >> "$LOG"
+fi
+
 # download the records
-echo "[INFO] Starting download"
+echo "[INFO] Starting download" >> "$LOG"
 python3.6 ./download_gsheet.py &>> "$LOG"
 
 # import or barf
@@ -38,12 +47,27 @@ if [ -e "$RECORDS" ]
 then
     echo "[INFO] Download successful" >> "$LOG"
 
-    # import the records
-    echo "[INFO] Importing records" >> "$LOG"
-    (cd $NHBAPPS; python3.6 manage.py import_records /tmp/downloader/records.json &>> "$LOG")
+    # calculate the hash so we can detect if it has not changed
+    HASH=$(sha1sum < "$RECORDS")
+    echo "[INFO] Hash of newly downloaded records: $HASH" >> "$LOG"
+
+    if [ "$HASH" != "$LATESTHASH" ]
+    then
+        # file has changed, so store and import!
+
+        SPOOLFILE="$SPOOLDIR/records_$STAMP.json"
+        echo "[INFO] Storing records file in $SPOOLFILE" >> "$LOG"
+        cp "$RECORDS" "$SPOOLFILE"
+
+        # import the records
+        echo "[INFO] Importing records" >> "$LOG"
+        (cd $NHBAPPS; python3.6 manage.py import_records "$SPOOLFILE") &>> "$LOG"
+    else
+        echo "[INFO] Records have not changed" >> "$LOG"
+    fi
 
     # clean up
-    echo "[INFO] Cleaning up"
+    echo "[INFO] Cleaning up" >> "$LOG"
     rm -rf "$TMPDIR" &>> "$LOG"
 else
     echo "[ERROR] Download failed: cannot locate $RECORDS" >> "$LOG"
