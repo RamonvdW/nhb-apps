6a011ed7bc9fb43862da7c17e95cd131f92118f6
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2020-01-29 22:45:36 +0100

    Added options for NL records and sending emails

--- nhb-apps/settings_local.py
+++ nhb-apps/settings_local.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019 Ramon van der Winkel.
+#  Copyright (c) 2019-2020 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -19,6 +19,8 @@ SECRET_KEY = '1234-replace-with-your-own-secret-key-56789abcdefg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+SITE_URL = "https://yourdomain.com"         # used by Overige:tijdelijke urls
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
@@ -39,6 +41,22 @@ DATABASES = {
 
 # the issuer name that is sent to the OTP application in the QR code
 OTP_ISSUER_NAME = "yoursite.com"
+
+# sending email via MailGun
+MAILGUN_URL = 'https://api.eu.mailgun.net/v3/yourdomain.com/messages'
+MAILGUN_API_KEY = 'mailgun private api key'
+EMAIL_FROM_ADDRESS = 'noreply@yourdomain.com'         # zie ook https://nl.wikipedia.org/wiki/Noreply
+
+# google doc id van het gsheet document
+RECORDS_GSHEET_FILE_ID = 'random google document number'
+
+# door de naam van een sheet te gebruiken as 'Range' krijg je alle cellen uit de sheet
+RECORDS_GSHEET_SHEET_NAMES = [
+    'Data individueel outdoor',
+    'Data individueel indoor',
+    'Data individueel 25m1pijl',
+    'Data team'
+]
 
 # url van de wiki server
 WIKI_URL = 'http://wiki.yourdomain.com'
