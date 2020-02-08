5082de8faebecdb083fa17bc42ca29d6115fbc93
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2020-02-08 18:02:01 +0100

    Added test for temporary urls

--- Overig/tests.py
+++ Overig/tests.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019 Ramon van der Winkel.
+#  Copyright (c) 2019-2020 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from Plein.tests import assert_html_ok, assert_other_http_commands_not_supported, assert_template_used
-from .models import SiteFeedback
+from .models import SiteFeedback, save_tijdelijke_url
 from Account.models import Account, account_zet_sessionvars_na_otp_controle
 from Account.rol import rol_zet_sessionvars_na_login
 
 
-class OverigTest(TestCase):
+class TestOverig(TestCase):
+    """ unit tests voor de Overig applicatie """
 
     def setUp(self):
         """ initializatie van de test case """
@@ -23,6 +24,9 @@ class OverigTest(TestCase):
         self.account_normaal = Account.objects.get(username='normaal')
         self.account_admin = Account.objects.get(username='admin')
         # TODO: add real feedback to the database, for better tests
+
+        save_tijdelijke_url('code1', geldig_dagen=-1)       # verlopen
+        save_tijdelijke_url('code2', geldig_dagen=1)        # no accountemail
 
     def test_feedback_get(self):
         resp = self.client.get('/overig/feedback/min/plein/')
@@ -143,6 +147,13 @@ class OverigTest(TestCase):
         resp = self.client.get('/overig/url/test/')
         self.assertEqual(resp.status_code, 404)
 
+    def test_tijdelijkeurl_verlopen(self):
+        resp = self.client.get('/overig/url/code1/')
+        self.assertEqual(resp.status_code, 404)
+
+    def test_tijdelijkeurl_geen_accountemail(self):
+        resp = self.client.get('/overig/url/code2/')
+        self.assertEqual(resp.status_code, 404)
 
 # TODO: add use of assert_other_http_commands_not_supported
 
