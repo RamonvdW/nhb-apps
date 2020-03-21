443f391521081fc102e13941583a9db763324875
RamonvdW <nhb.ramonvdw@gmail.com>
2020-03-21 22:13:39 +0100

    Added comments

--- check_feedback_op_pagina.sh
+++ check_feedback_op_pagina.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+#  Copyright (c) 2020 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+# helper to check the uniqueness of the "op_pagina" for the feedback form
+
 find . -name \*.dtl -exec grep "with op_pagina" {} \+ | cut -d':' -f2 | grep -E '^{' | cut -d' ' -f5 | sort | uniq -c | sort -n
+
 # end of file
