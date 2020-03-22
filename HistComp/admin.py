46869c710b4cab2a0b85b011dfff354c7df592df
RamonvdW <nhb.ramonvdw@gmail.com>
2020-03-22 20:05:04 +0100

    Enabled search

--- HistComp/admin.py
+++ HistComp/admin.py
@@ -5,11 +5,15 @@
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
 from django.contrib import admin
-
 from .models import HistCompetitie, HistCompetitieIndividueel, HistCompetitieTeam
 
+
+class HistCompetitieIndividueelAdmin(admin.ModelAdmin):
+    search_fields = ('schutter_nr', 'schutter_naam')
+
+
 admin.site.register(HistCompetitie)
-admin.site.register(HistCompetitieIndividueel)
+admin.site.register(HistCompetitieIndividueel, HistCompetitieIndividueelAdmin)
 admin.site.register(HistCompetitieTeam)
 
 # end of file
