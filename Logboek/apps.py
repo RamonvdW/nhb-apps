e914c864eec23f0512c2911a474c88c2adb067bf
RamonvdW <nhb.ramonvdw@gmail.com>
2020-04-13 10:39:26 +0200

    Avoiding problems when starting clean

--- Logboek/apps.py
+++ Logboek/apps.py
@@ -6,6 +6,20 @@
 
 from django.apps import AppConfig
 from django.conf import settings
+from django.db.models.signals import post_migrate
+
+
+def post_migration_callback(sender, **kwargs):
+    """ schrijf in het logboek na uitrol van een nieuwe versie """
+
+    msg = "Start met versie %s" % repr(settings.SITE_VERSIE)
+
+    # must import here to avoid AppRegistryNotReady exception
+    from .models import LogboekRegel, schrijf_in_logboek
+
+    # schrijf het nieuw versienummer in het logboek
+    if len(LogboekRegel.objects.filter(gebruikte_functie='Uitrol', activiteit=msg)) == 0:
+        schrijf_in_logboek(None, 'Uitrol', msg)  # pragma: no cover
 
 
 class LogboekConfig(AppConfig):
@@ -14,18 +28,8 @@ class LogboekConfig(AppConfig):
     name = 'Logboek'
 
     def ready(self):
+        post_migrate.connect(post_migration_callback, sender=self)
         # perform one-time startup logic
-
-        # must import here to avoid AppRegistryNotReady exception
-        from .models import LogboekRegel, schrijf_in_logboek
-
-        msg = "Start met versie %s" % repr(settings.SITE_VERSIE)
-
-        # TODO: code below causes exception and crash before initial migrations
-
-        # schrijf het nieuw versienummer in het logboek
-        if len(LogboekRegel.objects.filter(gebruikte_functie='Uitrol', activiteit=msg)) == 0:
-            schrijf_in_logboek(None, 'Uitrol', msg)         # pragma: no cover
 
 
 # end of file
