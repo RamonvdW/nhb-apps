d9fd2c9b4c0b2eee4dde16ce923379610784ade0
RamonvdW <nhb.ramonvdw@gmail.com>
2020-04-18 21:24:26 +0200

    Increased test coverage

--- Account/test_aanmaken.py
+++ Account/test_aanmaken.py
@@ -4,12 +4,8 @@
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
-from django.utils import timezone
 from django.test import TestCase
-from django.conf import settings
-from .models import Account, AccountEmail,account_is_email_valide
-from .views import obfuscate_email
-from .forms import LoginForm
+from .models import account_test_wachtwoord_sterkte
 from NhbStructuur.models import NhbRayon, NhbRegio, NhbVereniging, NhbLid
 from NhbStructuur.migrations.m0002_nhbstructuur_2018 import maak_rayons_2018, maak_regios_2018
 from Overig.e2ehelpers import E2EHelpers
@@ -91,6 +87,89 @@ class TestAccountAanmaken(E2EHelpers, TestCase):
 
         self.e2e_assert_other_http_commands_not_supported('/account/bevestigd/')
 
-# TODO: gebruik assert_other_http_commands_not_supported
+    def test_account_helpers(self):
+        account = self.account_normaal
+        account.first_name = "Normale"
+        account.last_name = "Tester"
+        account.save()
+        self.assertEqual(account.get_first_name(), 'Normale')
+        self.assertEqual(account.volledige_naam(), 'Normale Tester')
+        self.assertEqual(account.get_real_name(), 'Normale Tester')
+        self.assertEqual(account.get_account_full_name(), 'Normale Tester (normaal)')
+
+        account.first_name = ""
+        account.last_name = "Tester"
+        account.save()
+        self.assertEqual(account.get_first_name(), 'normaal')
+        self.assertEqual(account.volledige_naam(), 'Tester')
+        self.assertEqual(account.get_real_name(), 'Tester')
+        self.assertEqual(account.get_account_full_name(), 'Tester (normaal)')
+
+        account.first_name = "Normale"
+        account.last_name = ""
+        account.save()
+        self.assertEqual(account.get_first_name(), 'Normale')
+        self.assertEqual(account.volledige_naam(), 'Normale')
+        self.assertEqual(account.get_real_name(), 'Normale')
+        self.assertEqual(account.get_account_full_name(), 'Normale (normaal)')
+
+        account.first_name = ""
+        account.last_name = ""
+        account.save()
+        self.assertEqual(account.get_first_name(), 'normaal')
+        self.assertEqual(account.volledige_naam(), 'normaal')
+        self.assertEqual(account.get_real_name(), 'normaal')
+        self.assertEqual(account.get_account_full_name(), 'normaal (normaal)')
+
+    def test_email(self):
+        account = self.account_normaal
+        email = self.email_normaal
+        self.assertEqual(account.get_email(), 'normaal@test.com')
+
+        email.delete()
+        self.assertEqual(account.get_email(), '')
+
+    def test_wachtwoord_sterkte(self):
+        res, msg = account_test_wachtwoord_sterkte('xx', '')
+        self.assertEqual((res, msg), (False, "Wachtwoord moet minimaal 9 tekens lang zijn"))
+
+        res, msg = account_test_wachtwoord_sterkte('NHB123456', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord bevat een verboden reeks"))
+
+        res, msg = account_test_wachtwoord_sterkte('xxxxxXXXXX', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord bevat te veel gelijke tekens"))
+
+        res, msg = account_test_wachtwoord_sterkte('jajajaJAJAJA', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord bevat te veel gelijke tekens"))
+
+        res, msg = account_test_wachtwoord_sterkte('blablabla', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord bevat te veel gelijke tekens"))
+
+        res, msg = account_test_wachtwoord_sterkte('grootGROOT', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord bevat te veel gelijke tekens"))
+
+        res, msg = account_test_wachtwoord_sterkte('groteGROTE', '123456')
+        self.assertEqual((res, msg), (True, None))
+
+        res, msg = account_test_wachtwoord_sterkte('wachtwoord', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord is niet sterk genoeg"))
+
+        res, msg = account_test_wachtwoord_sterkte('mijnGeheim', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord is niet sterk genoeg"))
+
+        res, msg = account_test_wachtwoord_sterkte('HandBoogSport', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord is niet sterk genoeg"))
+
+        res, msg = account_test_wachtwoord_sterkte('qwertyuiop', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord is niet sterk genoeg"))
+
+        res, msg = account_test_wachtwoord_sterkte('234567890', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord is niet sterk genoeg"))
+
+        res, msg = account_test_wachtwoord_sterkte('zxcvbnm,.', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord is niet sterk genoeg"))
+
+        res, msg = account_test_wachtwoord_sterkte('passWORD!', '123456')
+        self.assertEqual((res, msg), (False, "Wachtwoord is niet sterk genoeg"))
 
 # end of file
