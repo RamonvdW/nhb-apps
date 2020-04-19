f3e4cff3fdcb923192b49cbf54cbe6466e3778e2
RamonvdW <nhb.ramonvdw@gmail.com>
2020-04-19 14:36:38 +0200

    Moved email related helper functions to Mailer app

--- Schutter/models.py
+++ Schutter/models.py
@@ -7,13 +7,12 @@
 
 from django.db import models
 from BasisTypen.models import BoogType
-from Account.models import Account, AccountEmail, AccountCreateError, account_is_email_valide
 from NhbStructuur.models import NhbLid
-from Overig.tijdelijke_url import maak_tijdelijke_url_accountemail
+from Account.models import Account
 # mag niet afhankelijk zijn van Competitie
 
 
-class AccountCreateNhbGeenEmail(Exception):
+class SchutterNhbLidGeenEmail(Exception):
     """ Specifieke foutmelding omdat het NHB lid geen e-mail adres heeft """
     pass
 
@@ -47,58 +46,6 @@ class SchutterBoog(models.Model):
 
     def __str__(self):
         return "%s - %s" % (self.nhblid.nhb_nr, self.boogtype.beschrijving)
-
-
-def account_create_nhb(nhb_nummer, email, nieuw_wachtwoord):
-    """ Maak een nieuw account aan voor een NHB lid
-        raises AccountError als:
-            - er al een account bestaat
-            - het nhb nummer niet valide is
-            - het email adres niet bekend is bij de nhb
-            - het email adres niet overeen komt
-        geeft de url terug die in de email verstuurd moet worden
-    """
-    if Account.objects.filter(username=nhb_nummer).count() != 0:
-        raise AccountCreateError('Account bestaat al')
-
-    # zoek het email adres van dit NHB lid erbij
-    try:
-        nhb_nr = int(nhb_nummer)
-    except ValueError:
-        raise AccountCreateError('Onbekend NHB nummer')
-
-    try:
-        nhblid = NhbLid.objects.get(nhb_nr=nhb_nr)
-    except NhbLid.DoesNotExist:
-        raise AccountCreateError('Onbekend NHB nummer')
-
-    if not account_is_email_valide(nhblid.email):
-        raise AccountCreateNhbGeenEmail()
-
-    if email != nhblid.email:
-        raise AccountCreateError('De combinatie van NHB nummer en email worden niet herkend. Probeer het nog eens.')
-
-    # maak het account aan
-    account = Account()
-    account.username = nhb_nummer
-    account.set_password(nieuw_wachtwoord)
-    account.save()
-
-    # maak het email record aan
-    mail = AccountEmail()
-    mail.account = account
-    mail.email_is_bevestigd = False
-    mail.bevestigde_email = ''
-    mail.nieuwe_email = email
-    mail.save()
-
-    # koppelen nhblid en account
-    nhblid.account = account
-    nhblid.save()
-
-    # maak de url aan om het e-mailadres te bevestigen
-    url = maak_tijdelijke_url_accountemail(mail, nhb_nummer=nhb_nummer, email=email)
-    return url
 
 
 # end of file
