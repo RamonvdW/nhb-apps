ec28f5126953e1ecd972b38cd978c822cda35f69
RamonvdW <nhb.ramonvdw@gmail.com>
2020-04-26 22:16:43 +0200

    Tests for new functionality

---
+++ Schutter/test_regiocompetitie.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2019-2020 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+from django.test import TestCase
+from Account.models import Account, AccountEmail
+from NhbStructuur.models import NhbRegio, NhbVereniging, NhbLid
+from Overig.e2ehelpers import E2EHelpers
+import datetime
+
+
+class TestSchutterRegiocompetitie(E2EHelpers, TestCase):
+    """ unit tests voor de Schutter applicatie; module Inschrijven/Uitschrijven Regiocompetitie """
+
+    test_after = ('Account', 'NhbStructuur', 'Competitie')
+
+    def setUp(self):
+        """ initialisatie van de test case """
+        self.account_admin = self.e2e_create_account_admin()
+        self.account_normaal = self.e2e_create_account('normaal', 'normaal@test.com', 'Normaal')
+        self.account_metmail = self.e2e_create_account('metmail', 'metmail@test.com', 'MetMail')
+
+        self.email_normaal = self.account_normaal.accountemail_set.all()[0]
+        self.email_metmail = self.account_metmail.accountemail_set.all()[0]
+
+        # afhankelijk van de rayon/regio's aangemaakt door NhbStructuur migratie
+
+        # maak een test vereniging
+        ver = NhbVereniging()
+        ver.naam = "Grote Club"
+        ver.nhb_nr = "1000"
+        ver.regio = NhbRegio.objects.get(pk=111)
+        # secretaris kan nog niet ingevuld worden
+        ver.save()
+
+        # maak een test lid aan
+        lid = NhbLid()
+        lid.nhb_nr = 100001
+        lid.geslacht = "M"
+        lid.voornaam = "Ramon"
+        lid.achternaam = "de Tester"
+        lid.email = "rdetester@gmail.not"
+        lid.geboorte_datum = datetime.date(year=1972, month=3, day=4)
+        lid.sinds_datum = datetime.date(year=2010, month=11, day=12)
+        lid.bij_vereniging = ver
+        lid.save()
+        self.nhblid1 = lid
+
+        # maak een test lid aan
+        lid = NhbLid()
+        lid.nhb_nr = 100002
+        lid.geslacht = "V"
+        lid.voornaam = "Ramona"
+        lid.achternaam = "de Testerin"
+        lid.email = ""
+        lid.geboorte_datum = datetime.date(year=1972, month=3, day=4)
+        lid.sinds_datum = datetime.date(year=2010, month=11, day=12)
+        lid.bij_vereniging = ver
+        lid.save()
+
+    # end of file
