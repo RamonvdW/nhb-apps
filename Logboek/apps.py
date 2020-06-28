720ed39b3217167dab5adda82cefb30787f35f5c
RamonvdW <nhb.ramonvdw@gmail.com>
2020-06-28 12:10:33 +0200

    Now reporting server name in roll-out line

--- Logboek/apps.py
+++ Logboek/apps.py
@@ -12,7 +12,7 @@ from django.db.models.signals import post_migrate
 def post_migration_callback(sender, **kwargs):
     """ schrijf in het logboek na uitrol van een nieuwe versie """
 
-    msg = "Start met versie %s" % repr(settings.SITE_VERSIE)
+    msg = "Start met versie %s op %s" % (repr(settings.SITE_VERSIE), repr(settings.SITE_URL))
 
     # must import here to avoid AppRegistryNotReady exception
     from .models import LogboekRegel, schrijf_in_logboek
@@ -28,8 +28,11 @@ class LogboekConfig(AppConfig):
     name = 'Logboek'
 
     def ready(self):
-        post_migrate.connect(post_migration_callback, sender=self)
         # perform one-time startup logic
+
+        # koppel aan de callback als het 'migrate' commando klaar is
+        # in de callback schrijven we een regel in het logboek
+        post_migrate.connect(post_migration_callback, sender=self)
 
 
 # end of file
