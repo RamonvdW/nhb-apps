a0cf50c8a9b968b9adb78a01d028ca65ae105948
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2020-07-25 11:16:39 +0200

    New command to force 2FA secret

---
+++ Account/management/commands/zet_2fa_geheim.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2020 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+# zet het 2FA geheim van een account, vanaf de commandline
+
+from django.core.management.base import BaseCommand
+from Account.models import Account
+from Logboek.models import schrijf_in_logboek
+
+
+class Command(BaseCommand):
+    help = "Zet het 2FA geheim van een account"
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', nargs=1,
+                            help="inlog naam")
+        parser.add_argument('geheim', nargs=1,
+                            help="2FA geheim (16 tekens)")
+
+    def handle(self, *args, **options):
+        username = options['username'][0]
+        geheim = options['geheim'][0]
+
+        if len(geheim) != 16:
+            self.stderr.write("Foutief 2FA geheim: moet 16 tekens zijn")
+            return
+
+        try:
+            account = Account.objects.get(username=username)
+        except Account.DoesNotExist as exc:
+            self.stderr.write("%s" % str(exc))
+        else:
+            account.otp_code = geheim
+            account.save()
+
+            # schrijf in het logboek
+            schrijf_in_logboek(account=None,
+                               gebruikte_functie="zet_2fa_geheim (command line)",
+                               activiteit="2FA geheim is opgeslagen voor account %s" % repr(username))
+
+            self.stdout.write("2FA is opgeslagen voor account %s" % repr(username))
+
+# end of file
