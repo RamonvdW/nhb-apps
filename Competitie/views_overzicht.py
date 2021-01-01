fb7c8bd34ef2388c91949ab8c2743aaca6ebe379
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2021-01-01 20:35:57 +0100

    Removed card to start new competition

--- Competitie/views_overzicht.py
+++ Competitie/views_overzicht.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2020 Ramon van der Winkel.
+#  Copyright (c) 2019-2021 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -134,7 +134,7 @@ class CompetitieOverzichtView(View):
             # als er nog geen competitie is voor het huidige jaar, geeft de BB dan de optie om deze op te starten
             beginjaar = models_bepaal_startjaar_nieuwe_competitie()
             context['nieuwe_seizoen'] = "%s/%s" % (beginjaar, beginjaar+1)
-            context['bb_kan_competitie_aanmaken'] = (0 == objs.filter(begin_jaar=beginjaar).count())
+            context['bb_kan_competitie_aanmaken'] = (objs.count() == 0)  # (0 == objs.filter(begin_jaar=beginjaar).count())
 
             if context['bb_kan_ag_vaststellen']:
                 # zoek uit wanneer dit voor het laatste gedaan is
