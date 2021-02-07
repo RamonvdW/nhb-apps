d4dcd9dbaded904ec854acf1486393ae90dab443
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2021-02-07 22:11:25 +0100

    Optimized queries

--- Schutter/admin.py
+++ Schutter/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2020 Ramon van der Winkel.
+#  Copyright (c) 2020-2021 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -12,8 +12,22 @@ class SchutterBoogAdmin(admin.ModelAdmin):
     """ Admin configuratie voor SchutterBoog klasse """
     search_fields = ('nhblid__nhb_nr', 'nhblid__voornaam', 'nhblid__achternaam')
 
+    list_select_related = True
+
+    autocomplete_fields = ('nhblid',)
+
+
+class SchutterVoorkeurenAdmin(admin.ModelAdmin):
+    """ Admin configuratie voor SchutterVoorkeuren klasse """
+
+    list_select_related = True
+
+    autocomplete_fields = ('nhblid',)
+
+    search_fields = ('nhblid__nhb_nr', 'nhblid__voornaam', 'nhblid__achternaam')
+
 
 admin.site.register(SchutterBoog, SchutterBoogAdmin)
-admin.site.register(SchutterVoorkeuren)
+admin.site.register(SchutterVoorkeuren, SchutterVoorkeurenAdmin)
 
 # end of file
