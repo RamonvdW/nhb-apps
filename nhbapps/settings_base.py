765f476976dfedce49611f9170f0be0d50b4f1e5
RamonvdW <50547870+RamonvdW@users.noreply.github.com>
2021-03-26 17:35:47 +0100

    Keeping date input disabled

--- nhbapps/settings_base.py
+++ nhbapps/settings_base.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(PROJ_DIR)
 
 # version of the site
 # this is used to keep site feedback separated by version
-SITE_VERSIE = '2021-03-25'
+SITE_VERSIE = '2021-03-26'
 
 # modules van de site
 INSTALLED_APPS = [
