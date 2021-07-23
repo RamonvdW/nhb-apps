79ee1a94f2427d1afdc541d7642cafce12bfe8e2
RamonvdW <nhb.ramonvdw@gmail.com>
2021-07-23 21:08:11 +0200

    Enabled searching

--- Mailer/admin.py
+++ Mailer/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2020 Ramon van der Winkel.
+#  Copyright (c) 2020-2021 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -8,6 +8,10 @@ from django.contrib import admin
 from .models import MailQueue
 
 
-admin.site.register(MailQueue)
+class MailQueueAdmin(admin.ModelAdmin):
+    search_fields = ('mail_to',)
+
+
+admin.site.register(MailQueue, MailQueueAdmin)
 
 # end of file
