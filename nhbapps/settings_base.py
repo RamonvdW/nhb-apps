3573a14438d1019eb289dd0f620471894e2642fd
RamonvdW <nhb.ramonvdw@gmail.com>
2021-08-05 16:23:26 +0200

    New command to find and wrongly selected individual classes

--- nhbapps/settings_base.py
+++ nhbapps/settings_base.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(PROJ_DIR)
 
 # version of the site
 # this is used to keep site feedback separated by version
-SITE_VERSIE = '2021-07-27'
+SITE_VERSIE = '2021-08-05'
 
 # modules van de site
 INSTALLED_APPS = [
