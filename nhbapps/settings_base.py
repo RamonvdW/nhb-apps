db0a1ed8cf75820c3d4e921bb53d420bcd52a510
RamonvdW <nhb.ramonvdw@gmail.com>
2021-09-30 12:02:04 +0200

    Correct opening date for new card

--- nhbapps/settings_base.py
+++ nhbapps/settings_base.py
@@ -205,7 +205,7 @@ COMPETITIES_START_WEEK = 37
 COMPETITIE_18M_LAATSTE_WEEK = 50        # week 37 t/m week 50
 COMPETITIE_25M_LAATSTE_WEEK = 11        # week 37 t/m week 11
 
-COMPETITIES_OPEN_RK_TEAMS_DAYS_AFTER = 0#4*7      # open 4 weeks after start week
+COMPETITIES_OPEN_RK_TEAMS_DAYS_AFTER = 4*7      # open 4 weeks after start week
 
 # maximum aantal resultaten dat een doorzoeking van de records terug geeft
 # dit voorkomt honderden resultaten bij het zoeken naar de letter e
