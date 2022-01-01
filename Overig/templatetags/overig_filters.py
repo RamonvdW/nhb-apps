aad37c54a5409fbd9e00ca06439a3ae9ea529a37
RamonvdW <nhb.ramonvdw@gmail.com>
2022-01-01 13:02:52 +0100

    Wrap long e-mail addresses

--- Overig/templatetags/overig_filters.py
+++ Overig/templatetags/overig_filters.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2020 Ramon van der Winkel.
+#  Copyright (c) 2019-2021 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -43,8 +43,39 @@ def filter_highlight(text, search_for):
     return mark_safe(new_text + text)
 
 
-# register the filter
+def filter_wbr_email(text):
+    """  wbr_email filter looks for places where the e-mail address could be wrapped
+         and insert a <wbr> html tag before that character.
+    """
+    new_text = ""
+
+    while len(text) > 0:
+        pos1 = text.find('.')
+        pos2 = text.find('@')
+
+        if pos1 >= 0:
+            if pos1 < pos2:
+                new_text += escape(text[:pos1]) + '<wbr>' + escape(text[pos1])
+                text = text[pos1+1:]
+                continue        # with the while
+
+        if pos2 >= 0:
+            new_text += escape(text[:pos2]) + '<wbr>' + escape(text[pos2])
+            text = text[pos2+1:]
+            continue            # with the while
+
+        # niets meer gevonden
+        new_text += escape(text)
+        text = ''
+
+    # while
+
+    return mark_safe(new_text)
+
+
+# register the filters
 register = template.Library()
 register.filter('highlight', filter_highlight)
+register.filter('wbr_email', filter_wbr_email)
 
 # end of file
