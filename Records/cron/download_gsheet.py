60402b5323eca26cbbdedac7781e028916b081b7
RamonvdW <nhb.ramonvdw@gmail.com>
2022-01-04 17:44:49 +0100

    Added exception handling

--- Records/cron/download_gsheet.py
+++ Records/cron/download_gsheet.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2021 Ramon van der Winkel.
+#  Copyright (c) 2019-2022 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -8,6 +8,7 @@
 
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
+import google.auth.exceptions
 import settings_local
 import json
 
@@ -20,18 +21,22 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 print("[INFO] Nationale Records downloader service")
 
-creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-gsheets_api = build('sheets', 'v4', credentials=creds).spreadsheets()
+try:
+    creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    gsheets_api = build('sheets', 'v4', credentials=creds).spreadsheets()
 
-# door de naam van een sheet te gebruiken as 'Range' krijg je alle cellen uit de sheet
-request = gsheets_api.values().batchGet(
-                spreadsheetId=settings_local.RECORDS_GSHEET_FILE_ID,
-                ranges=settings_local.RECORDS_GSHEET_SHEET_NAMES)
-result = request.execute()
+    # door de naam van een sheet te gebruiken as 'Range' krijg je alle cellen uit de sheet
+    request = gsheets_api.values().batchGet(
+                    spreadsheetId=settings_local.RECORDS_GSHEET_FILE_ID,
+                    ranges=settings_local.RECORDS_GSHEET_SHEET_NAMES)
+    result = request.execute()
 
-with open(OUTFILE, "w", encoding='utf-8') as jsonfile:
-    json.dump(result, jsonfile, ensure_ascii=False)
+    with open(OUTFILE, "w", encoding='utf-8') as jsonfile:
+        json.dump(result, jsonfile, ensure_ascii=False)
 
-print('[INFO] Written ' + OUTFILE)
+    print('[INFO] Written ' + OUTFILE)
+
+except google.auth.exceptions.TransportError as exc:
+    print('[ERROR] Error downloading gsheet: %s' % str(exc))
 
 # end of file
