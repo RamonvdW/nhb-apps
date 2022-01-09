1931fc5d16cd1d56ec4a3ce82268c6050d6603b2
RamonvdW <nhb.ramonvdw@gmail.com>
2022-01-09 15:49:50 +0100

    Maintaining output of lines in logfile

--- NhbStructuur/cron/download_and_import_crm.sh
+++ NhbStructuur/cron/download_and_import_crm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  Copyright (c) 2019-2021 Ramon van der Winkel.
+#  Copyright (c) 2019-2022 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
