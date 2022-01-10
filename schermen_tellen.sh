ff3d9bc2eeb2df6f825950b22c0c87435f5034d4
RamonvdW <nhb.ramonvdw@gmail.com>
2022-01-10 20:45:02 +0100

    Updated to new look

--- schermen_tellen.sh
+++ schermen_tellen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  Copyright (c) 2021 Ramon van der Winkel.
+#  Copyright (c) 2021-2022 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -14,6 +14,8 @@ EXCLUDE+="|logboek/common"
 EXCLUDE+="|competitie/tijdlijn"
 EXCLUDE+="|competitie/menu.dtl"
 EXCLUDE+="|handleiding/menu.dtl"
+EXCLUDE+="|site_layout.dtl"
+EXCLUDE+="|site_layout_minimaal.dtl"
 
 INCLUDES=$(grep '{% include' */templates/*/*dtl | grep -vE "$EXCLUDE")
 RES=$?
@@ -39,10 +41,13 @@ do
     grep -q 'class="row-nhb-blauw"' $scherm
     CHECK1=$?
     
-    grep -q '<h3 class="nhb-rood-text">' $scherm
+    grep -q 'class="nhb-blauw-hoofd"' $scherm
     CHECK2=$?
+
+    grep -q '<h3 class="nhb-rood-text">' $scherm
+    CHECK3=$?
     
-    if [ $CHECK1 -eq 0 -o $CHECK2 -eq 0 ]
+    if [ $CHECK1 -eq 0 -o $CHECK2 -eq 0 -o $CHECK3 -eq 0 ]
     then
         NEW_NR=$((NEW_NR + 1))
         TYPE='+'
