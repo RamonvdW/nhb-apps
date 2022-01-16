d37655f58dbe976f3a75c066eaa7ebddf00dde12
RamonvdW <nhb.ramonvdw@gmail.com>
2022-01-16 17:29:44 +0100

    Use correct membership field

--- Competitie/operations/wedstrijdcapaciteit.py
+++ Competitie/operations/wedstrijdcapaciteit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2021 Ramon van der Winkel.
+#  Copyright (c) 2021-2022 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -189,7 +189,7 @@ def bepaal_waarschijnlijke_deelnemers(afstand, deelcomp, wedstrijd):
     for deelnemer in deelnemers_indiv:
         sporterboog = deelnemer.sporterboog
         sporter = sporterboog.sporter
-        ver = sporter.bij_vereniging
+        ver = deelnemer.bij_vereniging
         boog = sporterboog.boogtype
 
         sporter = SimpleNamespace(
