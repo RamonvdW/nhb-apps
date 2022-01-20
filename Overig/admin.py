bd00a5208cea7580a5152cb6e87d3e266ade7618
RamonvdW <nhb.ramonvdw@gmail.com>
2022-01-20 19:13:29 +0100

    Reduced database queries

--- Overig/admin.py
+++ Overig/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2021 Ramon van der Winkel.
+#  Copyright (c) 2019-2022 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -80,7 +80,8 @@ class SiteTijdelijkeUrlAdmin(admin.ModelAdmin):
     # readonly voorkomt inladen van lange lijst met mogelijkheden
     # dit is ook meteen de volgorde waarin ze getoond worden
     readonly_fields = ('hoortbij_accountemail',
-                       'hoortbij_functie')
+                       'hoortbij_functie',
+                       'hoortbij_kampioenschap')
 
     list_select_related = ('hoortbij_functie',
                            'hoortbij_accountemail',
