31cb5b5cfb86ee0fbc5ee0cceebbabd3c3976a18
RamonvdW <nhb.ramonvdw@gmail.com>
2022-05-27 12:20:44 +0200

    Increased test coverage

---
+++ Betaal/migrations/m0008_admin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2022 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    """ Migratie class voor dit deel van de applicatie """
+
+    # volgorde afdwingen
+    dependencies = [
+        ('Betaal', 'm0007_transactie_bedragen'),
+    ]
+
+    # migratie functies
+    operations = [
+        migrations.AlterField(
+            model_name='betaalmutatie',
+            name='url_checkout',
+            field=models.CharField(blank=True, default='', max_length=200),
+        ),
+    ]
+
+# end of file
