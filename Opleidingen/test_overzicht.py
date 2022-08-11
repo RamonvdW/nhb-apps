1582c57e1d7c3f8e2652bb0dcd4582ca6cdb9c9c
RamonvdW <nhb.ramonvdw@gmail.com>
2022-08-11 21:18:07 +0200

    Added test coverage

--- Opleidingen/test_overzicht.py
+++ Opleidingen/test_overzicht.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2022 Ramon van der Winkel.
+#  Copyright (c) 2022 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
 from django.test import TestCase
 from Functie.models import Functie
-from NhbStructuur.models import NhbRayon, NhbRegio, NhbVereniging
-from Sporter.models import Sporter
 from TestHelpers.e2ehelpers import E2EHelpers
-import datetime
 
 
 class TestFunctieOverzicht(E2EHelpers, TestCase):
