6293d41827dda86c4a39aca5dafdd2973e633b8c
RamonvdW <nhb.ramonvdw@gmail.com>
2022-09-16 14:50:00 +0200

    Improved the 2FA trouble log

--- Account/otp.py
+++ Account/otp.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2021 Ramon van der Winkel.
+#  Copyright (c) 2019-2022 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -56,7 +56,7 @@ def account_otp_controleer(request, account, code):
     schrijf_in_logboek(account=None,
                        gebruikte_functie="OTP controle",
                        activiteit='Gebruiker %s OTP controle mislukt vanaf IP %s' % (repr(account.username), from_ip))
-    my_logger.info('%s 2FA mislukte controle voor account %s' % (from_ip, account.username))
+    my_logger.info('%s 2FA mislukt voor account %s met code %s' % (from_ip, account.username, repr(code)))
     return False
 
 
