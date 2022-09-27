b486e0b518294d279b147b498cc2e45e064bf563
RamonvdW <nhb.ramonvdw@gmail.com>
2022-09-27 22:26:38 +0200

    Updated feedback page references

--- check_feedback_op_pagina.sh
+++ check_feedback_op_pagina.sh
@@ -7,7 +7,7 @@
 # helper to check the uniqueness of the "op_pagina" for the feedback form
 
 DTL_INCLUDES=$(echo "/menu.dtl /andere-sites-van-de-nhb.dtl /site_layout.dtl /site_layout_minimaal.dtl /fout_403.dtl /fout_404.dtl /fout_500.dtl /competitie/tijdlijn.dtl /card.dtl /card_logo.dtl /card_nog-niet-beschikbaar /card_logo_nog-niet-beschikbaar /ga-naar-live-server.dtl" | tr ' ' '|')
-DTL_BEWUST_NIET=$(echo "/templates/email nhbapps/templates/snippets.dtl /plein-bezoeker.dtl /niet-ondersteund.dtl /feedback/ /tijdelijke-url- /Logboek/ /Handleiding/ /templates/plein/menu- /account/login-as-zoek.dtl /account/bevestigd.dtl /sporter/registreer-nhb-account.dtl" | tr ' ' '|')
+DTL_BEWUST_NIET=$(echo "/templates/email nhbapps/templates/snippets.dtl /plein-bezoeker.dtl /niet-ondersteund.dtl /feedback/ /tijdelijke-url- /Logboek/ /Handleiding/ /templates/plein/menu- /records_specifiek.dtl" | tr ' ' '|')
 
 DTL_FILES=$(find . -name \*.dtl | grep -vE "$DTL_BEWUST_NIET" | grep -vE "$DTL_INCLUDES")
 
@@ -19,9 +19,21 @@ ALL=' 1 op_pagina'
 
 grep "with op_pagina" $DTL_FILES | cut -d':' -f2 | tr ' ' '\n' | grep op_pagina | sort | uniq -c | sort -n | grep -v "$ALL"
 
+for dtl in $DTL_FILES;
+do
+  AppName=$(echo $dtl | cut -d/ -f2)
+  app_name=${AppName,,}
+  verwacht="op_pagina=\"$app_name-"
+  grep -q $verwacht $dtl
+  if [ $? -ne 0 ]
+  then
+      echo "[WARNING] $verwacht niet gevonden in $dtl"
+  fi
+done
+
 [ $# -gt 0 ] || echo "Gebruik -v om alles te krijgen"
 
 echo "[DEBUG] Geen 'op_pagina':"
-grep -L "with op_pagina" $DTL_FILES
+grep --files-without-match "with op_pagina" $DTL_FILES
 
 # end of file
