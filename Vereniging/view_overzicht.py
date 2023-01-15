fdc4c9961425fafeb7d8017aab9243f41d8a23d0
RamonvdW <nhb.ramonvdw@gmail.com>
2023-01-15 08:58:38 +0100

    Prevent showing card to WL that may not be used

--- Vereniging/view_overzicht.py
+++ Vereniging/view_overzicht.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2022 Ramon van der Winkel.
+#  Copyright (c) 2019-2023 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -196,7 +196,7 @@ class OverzichtView(UserPassesTestMixin, TemplateView):
             for deelkamp_rk in deelkamps_rk:
                 if deelkamp_rk.competitie == comp:
                     if deelkamp_rk.heeft_deelnemerslijst:
-                        if 'J' <= comp.fase <= 'K':
+                        if 'J' <= comp.fase <= 'K' and self.rol_nu != Rollen.ROL_WL:
                             # RK voorbereidende fase
                             kaartje = SimpleNamespace()
                             kaartje.titel = "Deelnemers RK"
