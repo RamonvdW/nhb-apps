5a4c280987e7aef1ea7e1e67e93515a1c2e28659
RamonvdW <nhb.ramonvdw@gmail.com>
2023-01-27 22:04:09 +0100

    Suppress deleted (hidden) locations

--- Vereniging/view_accommodatie.py
+++ Vereniging/view_accommodatie.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2022 Ramon van der Winkel.
+#  Copyright (c) 2019-2023 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -57,7 +57,7 @@ class AccommodatieDetailsView(UserPassesTestMixin, TemplateView):
         binnen_locatie = None
         buiten_locatie = None
         externe_locaties = list()
-        for loc in nhbver.wedstrijdlocatie_set.all():
+        for loc in nhbver.wedstrijdlocatie_set.exclude(zichtbaar=False):
             if loc.baan_type == BAAN_TYPE_EXTERN:
                 externe_locaties.append(loc)
             elif loc.baan_type == BAAN_TYPE_BUITEN:
