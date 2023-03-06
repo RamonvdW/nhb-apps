fc9db382eb45b3176803f692310e5f2aaf80e2fc
RamonvdW <nhb.ramonvdw@gmail.com>
2023-03-06 15:36:43 +0100

    Explanatory comment only

--- Account/forms.py
+++ Account/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2022 Ramon van der Winkel.
+#  Copyright (c) 2019-2023 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -52,6 +52,8 @@ class LoginForm(forms.Form):
             if login_naam == "" or wachtwoord == "":
                 self.add_error(None, 'Niet alle velden zijn ingevuld')
                 valid = False
+
+            # 'next' is checked when used, not here
 
         return valid
 
