9cae0ad6281ea3437e72eb127b7210b83a4dfcbe
RamonvdW <nhb.ramonvdw@gmail.com>
2023-05-24 21:31:14 +0200

    Import and publish BK 25m1p individual results

--- CompUitslagen/view_uitslagen_bk.py
+++ CompUitslagen/view_uitslagen_bk.py
@@ -127,7 +127,7 @@ class UitslagenBKIndivView(TemplateView):
                                           'sporterboog__sporter__bij_vereniging',
                                           'bij_vereniging')
                           .order_by('indiv_klasse__volgorde',
-                                    'result_volgorde',               # is constant zolang er geen resultaat is
+                                    'result_rank',
                                     'volgorde'))                     # inschrijf ranking
 
             for limiet in (KampioenschapIndivKlasseLimiet
