ef69ab6efe226168488460660ec29f6d1d292fb6
RamonvdW <nhb.ramonvdw@gmail.com>
2023-08-16 21:07:15 +0200

    Updated logo image aspect-ratio

--- Taken/views.py
+++ Taken/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2022 Ramon van der Winkel.
+#  Copyright (c) 2019-2023 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -145,7 +145,7 @@ class DetailsView(UserPassesTestMixin, TemplateView):
                 taak.log += "\n"
             taak.log += "[%s] %s heeft deze taak gesloten" % (timezone.now().strftime('%Y-%m-%d om %H:%M'), account)
             taak.is_afgerond = True
-            taak.save()
+            taak.save(update_fields=['log', 'is_afgerond'])
 
             eval_open_taken(request, forceer=True)
 
