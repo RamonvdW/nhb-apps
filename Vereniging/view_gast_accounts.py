ea1f7090be439e498001682e0660081e4f7d25ec
RamonvdW <nhb.ramonvdw@gmail.com>
2023-08-18 07:58:13 +0200

    Show matches with existing members

--- Vereniging/view_gast_accounts.py
+++ Vereniging/view_gast_accounts.py
@@ -12,6 +12,7 @@ from BasisTypen.definities import GESLACHT2STR
 from Functie.rol import rol_get_huidige_functie
 from Plein.menu import menu_dynamics
 from Registreer.models import GastRegistratie
+from Sporter.models import Sporter
 
 TEMPLATE_GAST_ACCOUNTS = 'vereniging/gast-accounts.dtl'
 TEMPLATE_GAST_ACCOUNT_DETAILS = 'vereniging/gast-account-details.dtl'
@@ -108,6 +109,75 @@ class GastAccountDetailsView(UserPassesTestMixin, TemplateView):
 
         gast.geslacht_str = GESLACHT2STR[gast.geslacht]
         context['gast'] = gast
+
+        # zoek naar overeenkomst in CRM
+        try:
+            eigen_lid_nr = int(gast.eigen_lid_nummer)
+        except ValueError:
+            pks1 = list()
+        else:
+            pks1 = list(Sporter
+                        .objects
+                        .exclude(is_gast=True)
+                        .filter(lid_nr=eigen_lid_nr)
+                        .values_list('pk', flat=True))
+
+        if gast.wa_id:
+            pks2 = list(Sporter
+                        .objects
+                        .exclude(is_gast=True)
+                        .filter(wa_id=gast.wa_id)
+                        .values_list('pk', flat=True))
+        else:
+            pks2 = list()
+
+        pks3 = list(Sporter
+                    .objects
+                    .exclude(is_gast=True)
+                    .filter(achternaam__iexact=gast.achternaam)
+                    .values_list('pk', flat=True))
+
+        pks4 = list(Sporter
+                    .objects
+                    .exclude(is_gast=True)
+                    .filter(voornaam__iexact=gast.voornaam)
+                    .values_list('pk', flat=True))
+
+        pks5 = list(Sporter
+                    .objects
+                    .exclude(is_gast=True)
+                    .filter(geboorte_datum=gast.geboorte_datum)
+                    .values_list('pk', flat=True))
+
+        pks6 = list(Sporter
+                    .objects
+                    .exclude(is_gast=True)
+                    .filter(email__iexact=gast.email)
+                    .values_list('pk', flat=True))
+
+        match_count = dict()    # [pk] = count
+        for pk in pks1 + pks2 + pks3 + pks4 + pks5 + pks6:
+            try:
+                match_count[pk] += 1
+            except KeyError:
+                match_count[pk] = 1
+        # for
+
+        best = list()
+        for pk, count in match_count.items():
+            tup = (count, pk)
+            best.append(tup)
+        # for
+        best.sort(reverse=True)     # hoogste eerst
+
+        beste_pks = [pk for count, pk in best[:10] if count > 1]
+
+        context['heeft_matches'] = len(beste_pks) > 0
+        context['sporter_matches'] = Sporter.objects.filter(pk__in=beste_pks)
+
+        for sporter in context['sporter_matches']:
+            sporter.geslacht_str = GESLACHT2STR[sporter.geslacht]
+        # for
 
         context['kruimels'] = (
             (reverse('Vereniging:overzicht'), 'Beheer Vereniging'),
