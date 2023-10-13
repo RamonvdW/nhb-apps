6a6ce3ecf93f1bbdb0f77a3f8dd252dddd143f4e
RamonvdW <nhb.ramonvdw@gmail.com>
2023-10-13 06:59:46 +0200

    Refactored location of test function

--- TestHelpers/template_status.py
+++ TestHelpers/template_status.py
@@ -4,6 +4,8 @@
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
+from pathlib import Path
+
 
 # template names checked out by assert_html_ok
 validated_templates = list()
@@ -30,5 +32,33 @@ included_templates = (
     'snippets.dtl',
     'email_mailer/email_basis.dtl',
 )
+
+
+def end_of_run():
+    """ Deze functie wordt aangeroepen aan het einde van een test run.
+        Hier rapporteren we welke templates niet gecheckt zijn met assert_html_ok.
+    """
+
+    # do something special for a "test all" run
+    if len(validated_templates) > 100:          # pragma: no branch
+        # for dtl in validated_templates:
+        #     print('Validated template: %s' % repr(dtl))
+        # # for
+
+        for dtl in Path().rglob('*.dtl'):
+            dtl_str = str(dtl)
+            is_email_template = '/templates/email_' in dtl_str
+            dtl_str = dtl_str[dtl_str.find('/templates/')+11:]
+            if dtl_str not in included_templates:       # pragma: no cover
+                if dtl_str not in validated_templates:
+                    if is_email_template:
+                        print('[WARNING] Missing assert_email_html_ok coverage for template %s' % repr(dtl_str))
+                    else:
+                        print('[WARNING] Missing assert_html_ok coverage for template %s' % repr(dtl_str))
+
+                if is_email_template and dtl_str not in consistent_email_templates:
+                    print('[WARNING] Missing assert_consistent_email_html_text coverage for e-mail template %s' % repr(dtl_str))
+        # for
+
 
 # end of file
