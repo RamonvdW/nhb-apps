9b57c8cd7261a2a77621dd10502083e6c85d09f3
RamonvdW <nhb.ramonvdw@gmail.com>
2023-10-15 12:04:09 +0200

    Renamed

---
+++ SiteMain/requirements/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#  Copyright (c) 2019-2023 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+# ga naar de directory waar het script staat
+SCRIPT_DIR=$(realpath "$0")             # volg links naar het echte script
+SCRIPT_DIR=$(dirname "$SCRIPT_DIR")     # directory van het script
+cd "$SCRIPT_DIR"
+
+for req in requirements requirements_dev
+do
+    OUT="$req.txt"
+    IN="$req.in"
+    echo "[INFO] Creating $OUT"
+    [ -f "$OUT" ] && rm "$OUT"
+    pip-compile --resolver=backtracking -q "$IN"
+done
+
+# end of file
+
