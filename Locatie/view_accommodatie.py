8793fd2780247897b79f206a7064595120ffe814
RamonvdW <nhb.ramonvdw@gmail.com>
2024-01-09 18:44:23 +0100

    Corrected location filtering

--- Locatie/view_accommodatie.py
+++ Locatie/view_accommodatie.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2023 Ramon van der Winkel.
+#  Copyright (c) 2019-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -69,7 +69,7 @@ class VerenigingAccommodatieDetailsView(UserPassesTestMixin, TemplateView):
         binnen_locatie = None
         buiten_locatie = None
         externe_locaties = list()
-        for loc in ver.locatie_set.all():
+        for loc in ver.locatie_set.exclude(zichtbaar=False):
             if loc.baan_type == BAAN_TYPE_EXTERN and loc.zichtbaar:
                 externe_locaties.append(loc)
             elif loc.baan_type == BAAN_TYPE_BUITEN:
