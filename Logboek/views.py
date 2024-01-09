12e08306727d936e38c21128c41ce39cb0c6efe8
RamonvdW <nhb.ramonvdw@gmail.com>
2024-01-09 18:16:35 +0100

    Added missing category

--- Logboek/views.py
+++ Logboek/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2023 Ramon van der Winkel.
+#  Copyright (c) 2019-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -34,6 +34,7 @@ DELEN = (
     ('rest', 'Rest'),
     ('records', 'Records'),
     ('accounts', 'Accounts'),
+    ('accommodaties', 'Accommodaties'),
     ('rollen', 'Rollen'),
     ('import', 'CRM import'),
     ('clusters', 'Clusters'),
