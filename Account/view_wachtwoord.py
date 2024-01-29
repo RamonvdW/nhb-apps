48957f011d3c5ac71e1acf2e5d1c08515eb086d0
RamonvdW <nhb.ramonvdw@gmail.com>
2024-01-29 20:16:17 +0100

    Corrected error log

--- Account/view_wachtwoord.py
+++ Account/view_wachtwoord.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2023 Ramon van der Winkel.
+#  Copyright (c) 2019-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -108,7 +108,7 @@ class WachtwoordVergetenView(TemplateView):
             schrijf_in_logboek(account=None,
                                gebruikte_functie="Wachtwoord",
                                activiteit="Stuur e-mail naar adres %s voor account %s, verzocht vanaf IP %s." % (
-                                           repr(account.email),         # kan bevestigde of nieuwe email zijn
+                                           repr(email),                 # kan bevestigde of nieuwe email zijn
                                            repr(account.get_account_full_name()),
                                            from_ip))
 
