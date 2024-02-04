352b49694bbed3db79935714006fb442fd33511a
RamonvdW <nhb.ramonvdw@gmail.com>
2024-02-04 09:34:12 +0100

    Typo

--- Records/tests/test_view.py
+++ Records/tests/test_view.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2023 Ramon van der Winkel.
+#  Copyright (c) 2019-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -281,7 +281,7 @@ class TestRecordsView(E2EHelpers, TestCase):
     def test_eervol(self):
         # eervolle vermelding
         AnderRecord(
-            titel='Guiness Book',
+            titel='Guinness Book',
             icoon='auto_awesome',
             tekst='test',
             url='https://www.handboogsport.nl').save()
