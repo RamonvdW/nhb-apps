f783d25ad54578c95cdf2183083eecdd127f3e07
RamonvdW <nhb.ramonvdw@gmail.com>
2024-03-11 17:32:29 +0100

    Handle club transfer during BK setup

--- Competitie/operations/overstappen.py
+++ Competitie/operations/overstappen.py
@@ -5,6 +5,7 @@
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
 from django.db.models import F
+from Competitie.definities import DEEL_RK
 from Competitie.models import KampioenschapSporterBoog
 
 """ Functionaliteit om de overstap van een sporter naar een andere vereniging af te handelen voor de bondscompetities
@@ -30,31 +31,46 @@ def competitie_hanteer_overstap_sporter(stdout):
         oude_vereniging = kampioen.bij_vereniging
         nieuwe_vereniging = kampioen.sporterboog.sporter.bij_vereniging
 
-        rayon_oud = oude_vereniging.regio.rayon_nr
-        rayon_nieuw = nieuwe_vereniging.regio.rayon_nr
-        binnen_zelfde_rayon = rayon_oud == rayon_nieuw
+        kamp_str = "[%s] %s (%s)" % (kampioen.sporterboog.sporter.lid_nr,
+                                     kampioen.sporterboog.sporter.volledige_naam(),
+                                     kampioen.sporterboog.boogtype.beschrijving)
 
-        # RK deelname: tijdens fase J en K mag de sporter overstappen binnen hetzelfde rayon
-        if comp.fase_indiv in ('J', 'K') and binnen_zelfde_rayon:
-            kamp_str = "[%s] %s (%s)" % (
-                        kampioen.sporterboog.sporter.lid_nr,
-                        kampioen.sporterboog.sporter.volledige_naam(),
-                        kampioen.sporterboog.boogtype.beschrijving)
+        accepted = False
+        rayon_str = ''
+        fase_str = ''
 
+        if kampioen.kampioenschap.deel == DEEL_RK:
+            # hanteer overstap RK deelnemer
+            rayon_oud = oude_vereniging.regio.rayon_nr
+            rayon_nieuw = nieuwe_vereniging.regio.rayon_nr
+            binnen_zelfde_rayon = rayon_oud == rayon_nieuw
+
+            # tijdens fase K en L van de competitie wordt de vereniging bevroren en moet
+            # de sporter uitkomen op het RK van het rayon waarin die vereniging valt
+
+            # tijdens fase J en K mag de sporter overstappen binnen hetzelfde rayon
+            if comp.fase_indiv in ('J', 'K') and binnen_zelfde_rayon:
+                accepted = True
+                rayon_str = ' in rayon %s' % rayon_oud
+                fase_str = 'J/K'
+        else:
+            # hanteer overstap BK deelnemer
+            # tijdens fase N en O mag de overstap verwerkt worden
+            if comp.fase_indiv in ('N', 'O'):
+                accepted = True
+                fase_str = 'N/O'
+
+        if accepted:
             stdout.write(
-                '[INFO] Overstap van [%s] naar [%s] in rayon %s tijdens fase J/K van %s geaccepteerd voor (pk=%s) %s' % (
-                            oude_vereniging.ver_nr, nieuwe_vereniging.ver_nr, rayon_oud,
-                            comp.beschrijving, kampioen.pk, kamp_str))
+                '[INFO] Overstap van [%s] naar [%s]%s tijdens fase %s van %s geaccepteerd voor (pk=%s) %s' % (
+                    oude_vereniging.ver_nr, nieuwe_vereniging.ver_nr, rayon_str, fase_str,
+                    comp.beschrijving, kampioen.pk, kamp_str))
 
             kampioen.bij_vereniging = nieuwe_vereniging
             kampioen.save(update_fields=['bij_vereniging'])
     # for
 
-    # RK deelname: tijdens fase K en L van de competitie wordt de vereniging bevroren en moet
-    #              de sporter uitkomen op het RK van het rayon waarin die vereniging valt
-
     # FUTURE: overschrijven tijdens regiocompetitie, bij afsluiten team ronde
-
 
 
 # end of file
