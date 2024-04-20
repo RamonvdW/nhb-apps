0d2ffb6814b293a3e09dd5c70a1b1aa3dbb64571
RamonvdW <nhb.ramonvdw@gmail.com>
2024-04-20 16:18:37 +0200

    Quality improvements

--- Sporter/apps.py
+++ Sporter/apps.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2021-2023 Ramon van der Winkel.
+#  Copyright (c) 2021-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -12,7 +12,7 @@ class SporterConfig(AppConfig):
 
     def ready(self):
         # laat de plugin zich registeren
-        import Sporter.plugins
+        import Sporter.plugins              # noqa
 
 
 # end of file
