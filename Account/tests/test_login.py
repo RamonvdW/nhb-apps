7f10dc73f4c6b17889cff5c0acc1184a9257911a
RamonvdW <nhb.ramonvdw@gmail.com>
2024-08-05 20:56:40 +0200

    Increased test coverage

--- Account/tests/test_login.py
+++ Account/tests/test_login.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2019-2023 Ramon van der Winkel.
+#  Copyright (c) 2019-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -321,6 +321,11 @@ class TestAccountLogin(E2EHelpers, TestCase):
         self.assert_html_ok(resp)
 
     def test_login_next(self):
+        with self.assert_max_queries(20):
+            resp = self.client.get(self.url_login, {'next': '/account/logout'})
+        self.assertEqual(resp.status_code, 200)     # 200 = OK
+        self.assert_html_ok(resp)
+
         # test een login met een 'next' parameter die na de login gevolgd wordt
         with self.assert_max_queries(22):
             resp = self.client.post(self.url_login, {'login_naam': 'metmail@test.com',
