5e0ef4feec6305966437877f875e46ce00c6c37a
RamonvdW <nhb.ramonvdw@gmail.com>
2024-09-11 19:43:52 +0200

    Show sessions

--- Bestelling/tests/test_overboeking.py
+++ Bestelling/tests/test_overboeking.py
@@ -348,11 +348,11 @@ class TestBestellingOverboeking(E2EHelpers, TestCase):
         self.assert_template_used(resp, ('bestelling/overboeking-ontvangen.dtl', 'plein/site_layout.dtl'))
 
         # juiste informatie en bevestiging van de gebruiker
-        self.assertEqual(self.bestelling2.status, BESTELLING_STATUS_BETALING_ACTIEF)
-        self.assertEqual(0, self.bestelling2.transacties.count())
+        self.assertEqual(self.bestelling.status, BESTELLING_STATUS_BETALING_ACTIEF)
+        self.assertEqual(0, self.bestelling.transacties.count())
         with self.assert_max_queries(20):
             resp = self.client.post(self.url_overboeking_ontvangen,
-                                    {'kenmerk': self.bestelling2.bestel_nr, 'bedrag': '1,23',
+                                    {'kenmerk': self.bestelling.bestel_nr, 'bedrag': '1,23',
                                      'actie': 'registreer', 'snel': '1'})
         self.assertEqual(resp.status_code, 200)     # 200 = OK
         self.assert_html_ok(resp)
@@ -363,23 +363,23 @@ class TestBestellingOverboeking(E2EHelpers, TestCase):
         # print('\nf1: %s\nf2: %s' % (f1.getvalue(), f2.getvalue()))
         self.assertTrue("[INFO] Overboeking 1.23 euro ontvangen voor bestelling MH-1235" in f2.getvalue())
         self.assertTrue("[INFO] Betaling is gelukt voor bestelling MH-1235" in f2.getvalue())
-        self.bestelling2 = Bestelling.objects.get(pk=self.bestelling2.pk)
-        self.assertEqual(self.bestelling2.status, BESTELLING_STATUS_AFGEROND)
-        self.assertEqual(1, self.bestelling2.transacties.count())
+        self.bestelling = Bestelling.objects.get(pk=self.bestelling.pk)
+        self.assertEqual(self.bestelling.status, BESTELLING_STATUS_AFGEROND)
+        self.assertEqual(1, self.bestelling.transacties.count())
 
     def test_afwijkend_bedrag(self):
         self.e2e_login_and_pass_otp(self.account_admin)
         self.e2e_wissel_naar_functie(self.functie_mww)
 
-        self.bestelling2.status = BESTELLING_STATUS_NIEUW
-        self.bestelling2.save(update_fields=['status'])
+        self.bestelling.status = BESTELLING_STATUS_NIEUW
+        self.bestelling.save(update_fields=['status'])
 
         # afwijkend bedrag
         self.assertEqual(0, BestellingMutatie.objects.count())
-        self.assertEqual(0, self.bestelling2.transacties.count())
+        self.assertEqual(0, self.bestelling.transacties.count())
         with self.assert_max_queries(20):
             resp = self.client.post(self.url_overboeking_ontvangen,
-                                    {'kenmerk': self.bestelling2.bestel_nr, 'bedrag': '1,00',   # moet zijn: 1,23
+                                    {'kenmerk': self.bestelling.bestel_nr, 'bedrag': '1,00',  # moet zijn: 1,23
                                      'actie': 'registreer', 'snel': '1'})
         self.assertEqual(resp.status_code, 200)     # 200 = OK
         self.assert_html_ok(resp)
@@ -387,11 +387,11 @@ class TestBestellingOverboeking(E2EHelpers, TestCase):
 
         self.assertContains(resp, 'Afwijkend bedrag')
         self.assertEqual(0, BestellingMutatie.objects.count())
-        self.assertEqual(0, self.bestelling2.transacties.count())
+        self.assertEqual(0, self.bestelling.transacties.count())
 
         with self.assert_max_queries(20):
             resp = self.client.post(self.url_overboeking_ontvangen,
-                                    {'kenmerk': self.bestelling2.bestel_nr, 'bedrag': '1,00',   # moet zijn: 1,23
+                                    {'kenmerk': self.bestelling.bestel_nr, 'bedrag': '1,00',  # moet zijn: 1,23
                                      'actie': 'registreer', 'accept_bedrag': 'ja', 'snel': '1'})
         self.assertEqual(resp.status_code, 200)     # 200 = OK
         self.assert_html_ok(resp)
@@ -401,8 +401,8 @@ class TestBestellingOverboeking(E2EHelpers, TestCase):
         f1, f2 = self.verwerk_bestel_mutaties()
 
         self.assertTrue("[INFO] Overboeking 1.00 euro ontvangen voor bestelling MH-1235" in f2.getvalue())
-        self.bestelling2 = Bestelling.objects.get(pk=self.bestelling2.pk)
-        self.assertEqual(self.bestelling2.status, BESTELLING_STATUS_BETALING_ACTIEF)
-        self.assertEqual(1, self.bestelling2.transacties.count())
+        self.bestelling = Bestelling.objects.get(pk=self.bestelling.pk)
+        self.assertEqual(self.bestelling.status, BESTELLING_STATUS_BETALING_ACTIEF)
+        self.assertEqual(1, self.bestelling.transacties.count())
 
 # end of file
