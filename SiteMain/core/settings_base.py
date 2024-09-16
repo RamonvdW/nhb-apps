965334e4c559761bc7749831c4ac57ef4a4d5131
RamonvdW <nhb.ramonvdw@gmail.com>
2024-09-16 07:29:06 +0200

    Corrected test cases

--- SiteMain/core/settings_base.py
+++ SiteMain/core/settings_base.py
@@ -58,7 +58,7 @@ BASE_DIR = os.path.dirname(PROJ_DIR)
 
 # version of the site
 # this is used to keep site feedback separated by version
-SITE_VERSIE = '2024-09-15'
+SITE_VERSIE = '2024-09-16'
 
 # modules van de site
 INSTALLED_APPS = [
