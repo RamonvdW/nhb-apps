38305b6863cdde727a3d3fe1e005c3c892c79b47
RamonvdW <nhb.ramonvdw@gmail.com>
2024-11-06 21:43:00 +0100

    Split dynamic url to avoid indexing

--- Kalender/urls.py
+++ Kalender/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2021-2023 Ramon van der Winkel.
+#  Copyright (c) 2021-2024 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -27,9 +27,17 @@ urlpatterns = [
          view_maand.KalenderMaandView.as_view(),
          name='maand'),
 
+    path('maand/<maand>-<int:jaar>/',
+         view_maand.KalenderMaandView.as_view(),
+         name='maand-simpel'),
+
     path('jaar/<maand>-<int:jaar>/<soort>/<bogen>/',
          view_jaar.KalenderJaarView.as_view(),
          name='jaar'),
+
+    path('jaar/<maand>-<int:jaar>/',
+         view_jaar.KalenderJaarView.as_view(),
+         name='jaar-simpel'),
 ]
 
 # end of file
