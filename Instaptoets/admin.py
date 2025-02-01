8293d5a0d0a65dfa0b3d07c42b908220c5064fb8
RamonvdW <nhb.ramonvdw@gmail.com>
2025-02-01 20:56:31 +0100

    Clean-up

--- Instaptoets/admin.py
+++ Instaptoets/admin.py
@@ -5,7 +5,7 @@
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
 from django.contrib import admin
-from Instaptoets.models import Categorie, Vraag, ToetsAntwoord, Instaptoets, Quiz, Uitdaging, VoorstelVraag
+from Instaptoets.models import Categorie, Vraag, ToetsAntwoord, Instaptoets, Quiz, Uitdaging
 
 
 class InstaptoetsAdmin(admin.ModelAdmin):
@@ -14,6 +14,10 @@ class InstaptoetsAdmin(admin.ModelAdmin):
 
     filter_horizontal = ('vraag_antwoord',)
 
+    search_fields = ('sporter__lid_nr',)
+
+    list_filter = ('geslaagd', 'aantal_goed')
+
 
 admin.site.register(Categorie)
 admin.site.register(Vraag)
@@ -21,6 +25,5 @@ admin.site.register(ToetsAntwoord)
 admin.site.register(Instaptoets, InstaptoetsAdmin)
 admin.site.register(Quiz)
 admin.site.register(Uitdaging)
-admin.site.register(VoorstelVraag)
 
 # end of file
