170804a877a92c768f7aa11749a917c3ea1ad68f
RamonvdW <nhb.ramonvdw@gmail.com>
2025-11-08 23:09:58 +0100

    Transition to svg icons

--- Plein/urls.py
+++ Plein/urls.py
@@ -32,10 +32,6 @@ urlpatterns = [
          views.TestUIView.as_view(),
          name='test-ui'),
 
-    path('test-icons/',
-         views.TestIconsView.as_view(),
-         name='test-icons'),
-
     path('test-speciale-pagina/<code>/',
          views_fout.TestSpecialePagina.as_view(),
          name='test-speciale-pagina'),
