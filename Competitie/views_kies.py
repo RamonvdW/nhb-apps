f984fa749dfcf897700a62831ee063262da6f4f6
RamonvdW <nhb.ramonvdw@gmail.com>
2025-12-31 20:20:29 +0100

    Google drive actions

--- Competitie/views_kies.py
+++ Competitie/views_kies.py
@@ -124,8 +124,9 @@ class CompetitieKiesView(TemplateView):
                 if not check_heeft_toestemming():
                     context['url_wf_toestemming_drive'] = reverse('CompBeheer:wf-toestemming-drive')
                 else:
+                    begin_jaar = Competitie.objects.order_by('begin_jaar').first()
                     if not aanmaken_wedstrijdformulieren_is_pending():
-                        for comp in Competitie.objects.exclude(regiocompetitie_is_afgesloten=True):
+                        for comp in Competitie.objects.filter(begin_jaar=begin_jaar):
                             count = aantal_ontbrekende_wedstrijdformulieren_rk_bk(comp)
                             if count > 0:
                                 context['url_wf_aanmaken'] = reverse('CompBeheer:wf-aanmaken')
