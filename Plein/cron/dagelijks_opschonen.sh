d4f39891452a26fc28a78938c83fcdabf5250f0e
RamonvdW <nhb.ramonvdw@gmail.com>
2026-02-11 22:12:37 +0100

    Script improvements

--- Plein/cron/dagelijks_opschonen.sh
+++ Plein/cron/dagelijks_opschonen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  Copyright (c) 2020-2023 Ramon van der Winkel.
+#  Copyright (c) 2020-2026 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -12,7 +12,7 @@ USER_WWW="$1"
 ID=$(id -u)
 ID_ROOT=$(id -u root)
 ID_WWW=$(id -u "$USER_WWW")
-if [ $ID -ne $ID_ROOT ] && [ $ID -ne $ID_WWW ]
+if [ "$ID" -ne "$ID_ROOT" ] && [ "$ID" -ne "$ID_WWW" ]
 then
     echo "Please run with sudo"
     exit 1
@@ -21,7 +21,7 @@ fi
 # ga naar de directory waar het script staat
 SCRIPT_DIR=$(realpath "$0")             # volg links naar het echte script
 SCRIPT_DIR=$(dirname "$SCRIPT_DIR")     # directory van het script
-cd "$SCRIPT_DIR"
+cd "$SCRIPT_DIR" || exit 1
 
 # move from Plein/cron/ to top-dir
 cd ../..
