b5cd5bdda619f039f253c2c835386605aaad9e6e
RamonvdW <nhb.ramonvdw@gmail.com>
2026-02-25 21:10:48 +0100

    Moved models into folder

---
+++ CompKampioenschap/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2026 Ramon van der Winkel.
+#  All rights reserved.
+#  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
+
+from .sheet_status import SheetStatus
+
+
+__all__ = ['SheetStatus',]
+
+
+# end of file
