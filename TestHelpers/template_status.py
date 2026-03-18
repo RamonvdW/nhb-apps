c81d13591ae53d9c205be40b96fe0f1be41f9ae5
RamonvdW <nhb.ramonvdw@gmail.com>
2026-03-18 19:30:12 +0100

    Javascript warning

--- TestHelpers/template_status.py
+++ TestHelpers/template_status.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright (c) 2020-2025 Ramon van der Winkel.
+#  Copyright (c) 2020-2026 Ramon van der Winkel.
 #  All rights reserved.
 #  Licensed under BSD-3-Clause-Clear. See LICENSE file for details.
 
@@ -24,6 +24,7 @@ included_templates = (
     'design/card_icon_beschikbaar-vanaf.dtl',
     'design/card_logo_beschikbaar-vanaf.dtl',
     'design/card_niet-beschikbaar.dtl',
+    'design/check_js_enabled.dtl',
     'plein/andere-sites.dtl',
     'plein/ga-naar-live-server.dtl',
     'feedback/sidebar.dtl',
