0693d4dda33b1168418bee51262f5f970f32fa56
RamonvdW <nhb.ramonvdw@gmail.com>
2026-09-13 10:51:56 +0200

    Prevent reporting empty accommodation

--- DataApi/view_ver.py
+++ DataApi/view_ver.py
@@ -41,6 +41,10 @@ class VerenigingenView(View):
                     .filter(ver_nr__in=ver_nrs_in_use)
                     .order_by('pk')):
 
+            # zonder postcode van de primary sportlocatie kunnen we deze niet rapporteren
+            if ver.postcode == '':
+                continue
+
             lijst.append(
                 {
                     "Verenigingscode": str(ver.ver_nr),
